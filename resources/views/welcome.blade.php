52838e6614752b71ee3ba8b4ec4c382562bf8edf
Dan Merfeld <dan@theorythree.com>
2019-12-06 10:32:17 +0200

    stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ben
+                    Ben and Dan are both designers.
                 </div>
 
                 <div class="links">
